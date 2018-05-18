2ed1c37670d5a25c955a0811326330ec8c7c62f6
grantnathanielbrown <32802164+grantnathanielbrown@users.noreply.github.com>
2018-05-18 17:46:47 -0400

    first commit

---
+++ blackjack.py
@@ -0,0 +1,12 @@
+# blackjack
+
+# need to make a deck with all cards
+# variable for player value and house value
+# command "hit" removes a card from deck array and adds it to player hand array.
+# playerScore = sum of card values
+
+
+# GLOBAL VARIABLES
+
+player_hand_score = 0
+dealer_hand_score = 0
